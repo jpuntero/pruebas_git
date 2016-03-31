b80e6d0eec247de893b7e405f3937f5e9a1a608e
JPUMU0J <JPUMU0J@ATJPUNTEROP.atsistemas.lan>
2016-03-31 18:00:54 +0200

    Primer commit

---
+++ pruebas_git/src/es/atsistemas/pruebas/MiClaseGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.atsistemas.pruebas;
+
+/**
+ * @author JPUMU0J
+ *
+ */
+public class MiClaseGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
